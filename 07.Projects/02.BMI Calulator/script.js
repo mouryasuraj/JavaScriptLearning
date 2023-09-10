3c7a62a1fa812667932cbb2f762f27707358e2fc
Suraj <mouryasuraj6266444@gmail.com>
2023-09-10 11:37:21 +0530

    added range

--- 07.Projects/02.BMI Calulator/script.js
+++ 07.Projects/02.BMI Calulator/script.js
@@ -6,6 +6,7 @@ const form = document.querySelector('form')
 const clear = document.querySelector('.clear')
 const height = document.querySelector('#height')
 const weight = document.querySelector('#weight')
+const range = document.querySelector('.range');
 // Events on Forms
 
 form.addEventListener('submit',(e)=>{
@@ -20,12 +21,22 @@ form.addEventListener('submit',(e)=>{
         results.innerHTML = `Please enter a valid weight in kg`
     }else{
         const bmi = (valOfWeight / ((valOfHeight * valOfHeight)/10000)).toFixed(2);
-        results.innerHTML = `<span>${bmi}</span>`
+        results.innerHTML = `<span>${bmi}</span>`;
+        if(bmi<18.5){
+            range.innerHTML = 'Under Weight'
+        }else if(bmi>=18.5 && bmi<25){
+            range.innerHTML = 'Normal Weight'
+        }else if(bmi>=25 && bmi<30){
+            range.innerHTML = 'Over Weight'
+        }else if(bmi>=30){
+            range.innerHTML = 'Obesity'
+        }
     }
 })
 
 // Event on clear button
 clear.addEventListener('click', ()=>{
+    range.innerHTML = ''
     height.value = '';
     weight.value = '';
 })
