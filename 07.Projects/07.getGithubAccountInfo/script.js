2642b9e891e133fdf711332eb445e15af8ecf0c4
Suraj <mouryasuraj6266444@gmail.com>
2023-09-19 22:45:54 +0530

    updated

--- 07.Projects/07.getGithubAccountInfo/script.js
+++ 07.Projects/07.getGithubAccountInfo/script.js
@@ -25,13 +25,16 @@ form.addEventListener('submit', (e) => {
 let userData;
 let createdAtdate;
 let updatedAtdate;
-async function fetchData() {
-    const requestURL = await `https://api.github.com/users/${valueOfUser}`;
+function fetchData() {
+    const requestURL =  `https://api.github.com/users/${valueOfUser}`;
     const xhr =  new XMLHttpRequest;
     xhr.open('GET', requestURL);
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
             userData = JSON.parse(this.responseText)
+            if(userData.message === 'Not Found'){
+                return alert('User is not found.')
+            }
         }
     }
     xhr.send()
@@ -47,22 +50,22 @@ function showData() {
     profileCard.innerHTML =
         `
         <img class="profileImg" src="${userData.avatar_url}" alt="">
-        <p> <span>Name:</span> ${userData.name}</p>    
-        <p> <span>Username:</span> ${userData.login}</p>    
-        <p> <span>Followers:</span> ${userData.followers}</p>    
-        <p> <span>Following:</span> ${userData.following}</p>    
-        <p> <span>Public Repository:</span> ${userData.public_repos}</p>    
-        <p> <span>Created at:</span> ${createdAtdate}</p>    
-        <p> <span>Updated at:</span> ${updatedAtdate}</p>    
-        <p> <span>Bio:</span> ${userData.bio}</p>    
-        <p> <span>Location:</span> ${userData.location}</p>    
+        <p> <span>Name:</span> ${userData.message === 'Not Found' ? 'Not found' : userData.name}</p>    
+        <p> <span>Username:</span> ${userData.message === 'Not Found' ? 'Not found' :userData.login}</p>    
+        <p> <span>Followers:</span> ${userData.message === 'Not Found' ? 'Not found' :userData.followers}</p>    
+        <p> <span>Following:</span> ${userData.message === 'Not Found' ? 'Not found' :userData.following}</p>    
+        <p> <span>Public Repository:</span> ${userData.message === 'Not Found' ? 'Not found' :userData.public_repos}</p>    
+        <p> <span>Created at:</span> ${userData.message === 'Not Found' ? 'Not found' :createdAtdate}</p>    
+        <p> <span>Updated at:</span> ${userData.message === 'Not Found' ? 'Not found' :updatedAtdate}</p>    
+        <p> <span>Bio:</span> ${userData.message === 'Not Found' ? 'Not found' :userData.bio}</p>    
+        <p> <span>Location:</span> ${userData.message === 'Not Found' ? 'Not found' :userData.location}</p>    
         <p> <span>Twitter:</span> <a target='_blank' href="https://twitter.com/${userData.twitter_username}">${userData.twitter_username}</a></p>    
-        <p> <span>Blog:</span> <a target='_blank' href="${userData.blog}">${userData.blog}</a></p>    
+        <p> <span>Blog:</span> <a target='_blank' href="${userData.message === 'Not Found' ? 'Not found' :userData.blog}">${userData.blog}</a></p>    
         <p> <span>Github Profile:</span> <a target='_blank' href="${userData.html_url}">${userData.html_url}</a></p> 
         `
 
 
-    // Logic to show modal when user clicks on profile image started
+    // Logic to show modal when user clicks on profile image
 
     // card Img
     const profileImg = document.querySelector('.profileImg')
@@ -90,7 +93,6 @@ function showData() {
     crossIcon.addEventListener('click', () => {
         modal.style.zIndex = '-1';
     })
-    // Logic to show modal when user clicks on profile image Ended
 }
 
 
